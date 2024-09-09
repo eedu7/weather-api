50030d84e1cd95f13d8eda12c8d1de55317d4bc7
eedu7 <mueedahmad067@gmail.com>
2024-09-09 11:21:41 +0500

    FEAT: added the caching service to catch data

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from fastapi import FastAPI, status, Depends
+from fastapi import Depends, FastAPI, status
 from fastapi.responses import JSONResponse
 from fastapi_limiter.depends import RateLimiter
 
-from utils import fetch_data, generate_url, lifespan
+from utils import (fetch_data, generate_url, get_cache_data, lifespan,
+                   set_cache_data)
 
-app = FastAPI(title="Weather API", lifespan=lifespan,)
+app = FastAPI(
+    title="Weather API",
+    lifespan=lifespan,
+)
 
 
-@app.get("/{city}", tags=["Fetch weather data"], dependencies=[Depends(RateLimiter(times=10, minutes=1))])
+@app.get(
+    "/{city}",
+    tags=["Fetch weather data"],
+    dependencies=[Depends(RateLimiter(times=10, minutes=1))],
+)
 async def home(
     city: str, start_date: str | None = None, end_date: str | None = None
 ) -> JSONResponse:
+    data = await get_cache_data(city)
+    if data:
+        return JSONResponse(status_code=status.HTTP_200_OK, content=data)
     url = await generate_url(city, start_date, end_date)
 
     data = await fetch_data(url)
+
+    await set_cache_data(city, data)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=data)
--- utils.py
+++ utils.py
@@ -1,47 +1,49 @@
+import json
 import os
 from contextlib import asynccontextmanager
+from math import ceil
+from time import sleep
 
+import redis.asyncio as redis
 import requests
 from dotenv import load_dotenv
-from fastapi import HTTPException, status, Request, Response, FastAPI
+from fastapi import FastAPI, HTTPException, Request, Response, status
 from fastapi_limiter import FastAPILimiter
 from requests.exceptions import HTTPError, RequestException, Timeout
-from math import ceil
-import redis.asyncio as redis
+
 load_dotenv()
 
 API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 API_KEY = os.getenv("API_KEY")
 REDIS_URL = os.getenv("REDIS_URL")
 
+redis = redis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+cache_expiry = 60 * 60 * 12
+
+
 async def generate_url(
-        city: str, start_date: str | None = None, end_date: str | None = None
+    city: str, start_date: str | None = None, end_date: str | None = None
 ):
     url = API_URL + city
     if start_date:
         url = url + f"/{start_date}"
     if end_date:
         url = url + f"/{end_date}"
-
-    complete_url = url + f"?key={API_KEY}"
-    return complete_url
+    return url + f"?key={API_KEY}"
 
 
 async def fetch_data(url: str):
     try:
         response = requests.get(url)
-
         response.raise_for_status()
+        sleep(30)
 
-        data = response.json()
-
-        return data
-
-    except HTTPError as e:
+        return response.json()
+    except HTTPError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key or url"
         )
-    except Timeout as e:
+    except Timeout:
         raise HTTPException(
             status_code=status.HTTP_504_GATEWAY_TIMEOUT,
             detail="Request is taking a long time to process",
@@ -52,20 +54,18 @@ async def fetch_data(url: str):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-async  def service_name_identifier(request: Request):
-    service = request.headers.get("Service-Name")
-    return service
+async def service_name_identifier(request: Request):
+    return request.headers.get("Service-Name")
+
 
 async def custom_callback(request: Request, response: Response, pexpire: int):
     expire = ceil(pexpire / 1000)
-
     raise HTTPException(
         status.HTTP_429_TOO_MANY_REQUESTS,
         f"Too many requests. Retry after {expire} seconds",
-        headers={
-            "Retry-After": str(expire)
-        }
+        headers={"Retry-After": str(expire)},
     )
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -73,6 +73,18 @@ async def lifespan(_: FastAPI):
     await FastAPILimiter.init(
         redis=redis_connection,
         identifier=service_name_identifier,
-        http_callback=custom_callback
+        http_callback=custom_callback,
     )
     yield await FastAPILimiter.close()
+
+
+async def get_cache_data(city: str):
+    json_data = await redis.get(city)
+    if json_data:
+        return json.loads(json_data)
+
+
+async def set_cache_data(city: str, value: dict):
+    json_value = json.dumps(value)
+    await redis.set(city, json_value, ex=cache_expiry)
+    return True
